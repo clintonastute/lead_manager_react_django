b45d19ad745e9cd87792cf0f4feefaa397baa679
clintonastute <astute.yard@gmail.com>
2019-06-01 21:19:24 +0545

    Clear Leads on Logout

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from '../actions/types.js';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, CLEAR_LEADS } from '../actions/types.js';
 
 const initialState = {
     leads: []
@@ -12,7 +12,7 @@ export default function (state = initialState, action) {
                 leads: action.payload
             }
         case DELETE_LEAD:
-            return{
+            return {
                 ...state,
                 leads: state.leads.filter(lead => lead.id !== action.payload)
             }
@@ -21,6 +21,11 @@ export default function (state = initialState, action) {
                 ...state,
                 leads: [...state.leads, action.payload]
             }
+        case CLEAR_LEADS:
+            return {
+                ...state,
+                leads: []
+            };
         default:
             return state;
     }
